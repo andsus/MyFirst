5b5839102e5282f46d4332ef889745ed9962e21e
Andre Susantin <susantin@amazon.com>
2024-04-12 20:44:55 -0700

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Susantin, Andre on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
